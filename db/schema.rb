3140dce3f450aca83f26cba555e5b486cf9531f1
Ivan-Sizyh <ivansizyh26@gmail.com>
2023-12-22 20:15:08 +0700

    rubocop

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,57 +12,57 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_22_121248) do
+ActiveRecord::Schema[7.0].define(version: 20_231_222_121_248) do # rubocop:disable Metrics/BlockLength
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "answers", force: :cascade do |t|
-    t.string "text", null: false
-    t.string "response_date", null: false
-    t.string "responder", null: false
-    t.bigint "question_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["question_id"], name: "index_answers_on_question_id"
+  create_table 'answers', force: :cascade do |t|
+    t.string 'text', null: false
+    t.string 'response_date', null: false
+    t.string 'responder', null: false
+    t.bigint 'question_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['question_id'], name: 'index_answers_on_question_id'
   end
 
-  create_table "questions", force: :cascade do |t|
-    t.string "text"
-    t.bigint "vacancy_id", null: false
-    t.bigint "tag_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.text "additional_field"
-    t.string "additioner"
-    t.index ["tag_id"], name: "index_questions_on_tag_id"
-    t.index ["vacancy_id"], name: "index_questions_on_vacancy_id"
+  create_table 'questions', force: :cascade do |t|
+    t.string 'text'
+    t.bigint 'vacancy_id', null: false
+    t.bigint 'tag_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.text 'additional_field'
+    t.string 'additioner'
+    t.index ['tag_id'], name: 'index_questions_on_tag_id'
+    t.index ['vacancy_id'], name: 'index_questions_on_vacancy_id'
   end
 
-  create_table "tags", force: :cascade do |t|
-    t.string "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'tags', force: :cascade do |t|
+    t.string 'title'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "vacancies", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'vacancies', force: :cascade do |t|
+    t.string 'title'
+    t.text 'description'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "video_links", force: :cascade do |t|
-    t.string "text", null: false
-    t.string "video_id", null: false
-    t.string "timecode", null: false
-    t.bigint "question_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["question_id"], name: "index_video_links_on_question_id"
+  create_table 'video_links', force: :cascade do |t|
+    t.string 'text', null: false
+    t.string 'video_id', null: false
+    t.string 'timecode', null: false
+    t.bigint 'question_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['question_id'], name: 'index_video_links_on_question_id'
   end
 
-  add_foreign_key "answers", "questions"
-  add_foreign_key "questions", "tags"
-  add_foreign_key "questions", "vacancies"
-  add_foreign_key "video_links", "questions"
+  add_foreign_key 'answers', 'questions'
+  add_foreign_key 'questions', 'tags'
+  add_foreign_key 'questions', 'vacancies'
+  add_foreign_key 'video_links', 'questions'
 end
